38160c42505440ac77e9921f5dd5be5f65017b38
K. Liutskevich <k.liutskevich@gmail.com>
2018-08-26 19:22:06 +0300

    новости (внутренняя)

--- bitrix/templates/main/components/bitrix/news/news/bitrix/news.detail/.default/template.php
+++ bitrix/templates/main/components/bitrix/news/news/bitrix/news.detail/.default/template.php
@@ -6,21 +6,24 @@ $this->setFrameMode(true);
 <?$this->SetViewTarget('header_block_title');?>
 <div class="h1"><?=($arResult["SECTION_INFO_NAME"]?$arResult["SECTION_INFO_NAME"]:$arResult["IBLOCK"]["NAME"])?></div>
 <?$this->EndViewTarget();?>
-<div class="t1 t1--gray advanced-page__news-date"><?=ToLower($arResult["DISPLAY_ACTIVE_FROM"])?></div>
-<a class="link-prev" href="<?=$arParams["SEF_FOLDER"]?>">
-    <div class="back-button">
-        <div class="back-button__wrapper">
-            <div class="back-button__image">
-                <svg xmlns="http://www.w3.org/2000/svg" viewbox="0 0 8.1 13.4">
-                    <path d="M1.4 13.4L0 12l5.3-5.3L0 1.4 1.4 0l6.7 6.7z"/>
-                </svg>
-            </div>
-            <div class="back-button__text">
-                 <?=GetMessage("NEWS_PAGE_LINK_PREV")?>
+<div class="aside-date-wrap">
+    <div class="t1 t1--gray advanced-page__news-date"><?=ToLower($arResult["DISPLAY_ACTIVE_FROM"])?></div>
+    <a class="link-prev" href="<?=$arParams["SEF_FOLDER"]?>">
+        <div class="back-button">
+            <div class="back-button__wrapper">
+                <div class="back-button__image">
+                    <svg height="9" xmlns="http://www.w3.org/2000/svg" viewbox="0 0 8.1 13.4">
+                        <path d="M1.4 13.4L0 12l5.3-5.3L0 1.4 1.4 0l6.7 6.7z"/>
+                    </svg>
+                </div>
+                <div class="back-button__text">
+                    <?=GetMessage("NEWS_PAGE_LINK_PREV")?>
+                </div>
             </div>
         </div>
-    </div>
-</a>
+    </a>
+</div>
+
 <div class="news-page-inner-item">
     <h1 class="h2"><?=$arResult["~NAME"]?></h1>
     <?if($arResult["RESIZE_IMG"]):?>
