36b252df2d6cca6b98dbfb13c77ec66136071c65
K. Liutskevich <k.liutskevich@gmail.com>
2018-08-26 20:24:05 +0300

    контакты

--- contacts/index_title_text.php
+++ contacts/index_title_text.php
@@ -1,3 +1,3 @@
-<div class="red-button--wrapper">
+<div class="red-button--wrapper contact-button">
 	<a class="red-button red-button--fill red-button--small" href="/shops/">
 <div class="red-button__inner">Адреса наших магазинов</div></a></div>
